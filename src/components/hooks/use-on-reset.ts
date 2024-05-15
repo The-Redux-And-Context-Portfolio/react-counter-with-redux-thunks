9b7edde65c43c5c16c8f1f9c253a567b62f2001a
Sricharan Krishnan <sricharan.krishnan@gmail.com>
2024-05-15 09:28:00 +0530

    Update use-on-reset.ts

--- src/components/hooks/use-on-reset.ts
+++ src/components/hooks/use-on-reset.ts
@@ -4,13 +4,18 @@ import { counterReset } from "../../redux-store/thunks/counter";
 import getSound from "../../redux-store/selectors/get-sound";
 import playResetSound from "../utils/reset-sound";
 
-/* hook */
+/**
+  * @returns - an event handler that helps reset the value of the counter to zero
+  */
 function useOnReset() {
+  /* subscribe to redux store updates */
   const sound = useAppSelector(getSound);
+  /* create a dispatch function to send instructions to the store */
   const reduxDispatch = useAppDispatch();
 
+  /* event handler */
   async function handleOnReset() {
-    await reduxDispatch(counterReset()).unwrap();
+    await reduxDispatch(counterReset());
     sound && playResetSound();
   }
 
