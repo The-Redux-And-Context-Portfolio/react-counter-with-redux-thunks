0879386136472a245a2a86fef19564972c342015
Sricharan Krishnan <sricharan.krishnan@gmail.com>
2024-05-15 09:18:01 +0530

    Update loader.ts

--- src/redux-store/reducers/loader.ts
+++ src/redux-store/reducers/loader.ts
@@ -2,7 +2,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { counterIncrement, counterDecrement, counterReset } from "../thunks/counter";
 
-/* reducer */
+/**
+ * the reducer that takes care of the value of the loader slice. this will be controlled based on the
+ * action that we are sending into the reducer
+ **/
 const initialState = false;
 const loaderSlice = createSlice({
   name: "loader",
