ab6f2b3c425c11c0ee27aaee900206fed09fbd84
Sricharan Krishnan <sricharan.krishnan@gmail.com>
2024-05-15 09:20:36 +0530

    Update counter.ts

--- src/redux-store/thunks/counter.ts
+++ src/redux-store/thunks/counter.ts
@@ -1,3 +1,10 @@
+/**
+  * Note: createAsyncThunks is not required for this type of application, but the reason why this has been used is 
+  * to be able to understand how the system works with thunks.
+  * The 'delayedPromiseResolver()' is used to provide the effect of a delayed state updated like people would experience 
+  * when making an api call.
+  */
+
 /* node modules */
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
