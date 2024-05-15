31c8bb28bccea8b2239ae4a90f341179a9c40667
Sricharan Krishnan <sricharan.krishnan@gmail.com>
2024-05-15 09:24:55 +0530

    Update index.tsx

--- src/components/counter/index.tsx
+++ src/components/counter/index.tsx
@@ -8,12 +8,17 @@ import getCounter from "../../redux-store/selectors/get-counter";
 
 /* component */
 function Counter(): JSX.Element {
+  /* subscribe to state updates from the redux store */
   const counter = useAppSelector(getCounter);
+  /* subscribe to updates from a custom hook */
   const { numberName } = useNumberName(counter);
+  
   return (
     <>
       <div className="posContainer text-center">
+        {/* Counter Value */}
         <span className="num" data-testid="counterElem">{ counter }</span>
+        {/* Counter Name - in words */}
         <span className="name" data-testid="counterNameElem">{ numberName }</span>
       </div>
     </>
